a405423c259644069ea38e24e793146e0694c876
askilton1 <antonio.skilton@gmail.com>
2016-09-10 16:40:42 -0700

    in the midst of creating dummy variables to permit data be used in linear model

--- cleanAndManipulate.R
+++ cleanAndManipulate.R
@@ -1,6 +1,6 @@
 cleanAndManipulate <- function(raw){
 
-raw$familySize <- train$Parch + train$SibSp
+raw$familySize <- raw$Parch + raw$SibSp
   
 raw$title <- NA
 for(string in c("master","mr","miss","mrs")) raw$title[grepl(string,raw$Name,ignore.case=T)] <- string
@@ -23,20 +23,31 @@ set.seed(13343)
 preObj <- preProcess(raw[,-2],method="knnImpute")
 Embarked <- predict(preObj,raw[,-2])$Embarked
 
+raw$adultMale <- 0; raw$adultFemale <- 0; raw$child <- 0 
+raw$adultMale[is.na(raw$Age) & as.character(raw$Sex) == "male"] <- 1
+raw$adultFemale[is.na(raw$Age) & as.character(raw$Sex) == "female"] <- 1
+raw$child[raw$Age<=10 | (raw$Age <= 15 & raw$Parch>0) | raw$SibSp > 1 | raw$title=="master"] <- 1
+
+ifelse(is.na(raw$Age),as.character(raw$Sex),
+       ifelse(raw$Age<=10 | (raw$Age <= 15 & raw$Parch>0),"child",as.character(raw$Sex)))
+
 library(dplyr)
 raw<-tbl_df(raw)
 raw<-raw %>%
-  mutate(sex_age = ifelse(is.na(Age),as.character(Sex),ifelse(Age<=10 | (Age <= 15 & Parch>0),"child",as.character(Sex)))) %>%
-  select(-title,-Pclass,-cabin_letter.G,-cabin_letter.A,-cabin_letter.T,-cabin_letter.C) %>%
-  select(-Cabin) %>%
-#  select(Survived,Sex,sex_age,contains("title"),contains("Pclass"),contains("cabin_letter"),Embarked,Fare,Parch,familySize) %>%
-  # mutate_at(as.factor) %>%
+  dplyr::select(-title,-Pclass,-cabin_letter.G,-cabin_letter.A,-cabin_letter.T,-cabin_letter.C,-sex_age) %>%
+  dplyr::select(-Sex) %>%
+  #dplyr::select(-Cabin,-Ticket,-Name,cabin_letter.B) %>%
+  dplyr::select(Survived,
+         #Sex,
+         contains("sex_age"),contains("title"),contains("Pclass"),contains("cabin_letter"),Embarked,Fare,Parch,familySize,
+         PassengerId) %>%
+  #mutate_at(as.factor) %>%
   na.omit %>%
   mutate_if(function(col) ifelse(is.factor(col),FALSE,max(col)==1 & min(col)==0),as.factor)
 raw$Fare <- as.numeric(as.character(raw$Fare)) 
 
 clean <- list() 
-clean$predictors <- raw %>% select(-Survived) 
-clean$Survived <- raw %>% select(Survived) %>% mutate_all(as.factor) 
+clean$predictors <- raw %>% dplyr::select(-Survived) 
+clean$Survived <- raw %>% dplyr::select(Survived) %>% mutate_all(as.factor) 
 
 return(clean)} 
--- manipulating.R
+++ manipulating.R
@@ -2,49 +2,31 @@ rm(list=ls())
 library(dplyr)
 train <- tbl_df(read.csv("train.csv")) 
 test <- tbl_df(read.csv("test.csv")) 
-
-train$title <- NA
-for(string in c("master","mr","miss","mrs")) train$title[grepl(string,train$Name,ignore.case=T)] <- string
-train$title <- as.factor(train$title)
-train$title[is.na(train$title)] <- ifelse(train$Sex[is.na(train$title)]=="female","mrs","mr")
-
-train$cabin_letter <- "U"
-for(letter in c("A","B","C","D","E","F","T","G")) train$cabin_letter[grepl(letter,train$Cabin)] <- letter
-train$cabin_letter <- as.factor(train$cabin_letter)
-train <- cbind(train,predict(dummyVars(~cabin_letter, data = train), newdata = train))
-attach(train);table(Survived,cabin_letter);detach(train)
-
-require(caret)
-train$Pclass <- as.factor(train$Pclass)
-train <- cbind(train,predict(dummyVars(~Pclass, data = train), newdata = train))
-train$title <- as.factor(train$title)
-train <- cbind(train,predict(dummyVars(~title, data = train), newdata = train))
-
-#Imputing Data
-train$Embarked[train$Embarked==""] <- NA
-library(caret)
-set.seed(13343)
-preObj <- preProcess(train[,-2],method="knnImpute")
-Embarked <- predict(preObj,train[,-2])$Embarked
-
-train.rf<-train %>%
-  mutate(sex_age = ifelse(is.na(Age),as.character(Sex),ifelse(Age<=10 | (Age <= 15 & Parch>0),"child",as.character(Sex)))) %>%
-  select(-title,-Pclass
-         #,-familysize
-         ) %>%
-  select(Survived,Sex,sex_age,contains("title"),contains("Pclass"),contains("cabin_letter"),#contains("family"),
-         Embarked,
-         Fare,Parch) %>%
-  mutate_all(as.factor) %>%
-  na.omit() 
-train.rf$Fare <- as.numeric(as.character(train.rf$Fare))
+source("cleanAndManipulate.R")
+predictors <- cleanAndManipulate(train)$predictors
+Survived <- cleanAndManipulate(train)$Survived[[1]]
 
 library(randomForest)
 set.seed(1)
-rf <- randomForest(Survived~.,data=train.rf,importance=T)
-summary(glm(Survived~.,data=train.rf,family="binomial"))
-rf$importance
+rf <- randomForest(predictors,Survived,importance=T)
+# set.seed(1)
+# rf_model<-train(Survived~.,data=cleanAndManipulate(train),method="rf",
+#                 trControl=trainControl(method="cv",number=10),
+#                 prox=TRUE,allowParallel=TRUE)
+
+# predict(rf,cleanAndManipulate(test)$predictors)
+
+imp <- importance(rf, type=1)
+featureImportance <- data.frame(Feature=row.names(imp), Importance=imp[,1])
+
+p <- ggplot(featureImportance, aes(x=reorder(Feature, Importance), y=Importance)) +
+  geom_bar(stat="identity", fill="#53cfff") +
+  coord_flip() + 
+  theme_light(base_size=20) +
+  xlab("") +
+  ylab("Importance") + 
+  ggtitle("Random Forest Feature Importance\n") +
+  theme(plot.title=element_text(size=18))
+p
 rf
-
-
-# submission$Survived <- predict(rf, extractFeatures(test))
+# ggsave("2_feature_importance.png", p)
