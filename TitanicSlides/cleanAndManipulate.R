592247b7c13eea3b847ce45f951e54c61799e28c
askilton1 <antonio.skilton@gmail.com>
2016-09-11 14:03:29 -0700

    initial commit with slides implemented. cleanAndManipulate.R has been modified as mistakes have been corrected. most significantly, Ticket_group has been turned into dummy groups, this it is not clear whether or not they can decrease error

--- TitanicSlides/cleanAndManipulate.R
+++ TitanicSlides/cleanAndManipulate.R
@@ -1,26 +1,29 @@
 cleanAndManipulate <- function(raw,linear=FALSE){
 raw$familySize <- raw$Parch + raw$SibSp
-  
+
+#title
 raw$title <- NA
 for(string in c("master","mr","miss","mrs")) raw$title[grepl(string,raw$Name,ignore.case=T)] <- string
-raw$title <- as.factor(raw$title)
 raw$title[is.na(raw$title)] <- ifelse(raw$Sex[is.na(raw$title)]=="female","mrs","mr")
 require(caret)
+raw$title <- as.factor(raw$title)
 raw <- cbind(raw,predict(dummyVars(~title, data = raw), newdata = raw))
+#Pclass
 raw$Pclass <- as.factor(raw$Pclass);raw <- cbind(raw,predict(dummyVars(~Pclass, data = raw), newdata = raw))
 raw <- cbind(raw,predict(dummyVars(~Embarked, data = raw), newdata = raw))
-
+#cabin
 raw$cabin <- 0
 raw$cabin[raw$Cabin==""] <- 1
-#for(letter in c("A","B","C","D","E","F","T","G")) raw$cabin[grepl(letter,raw$Cabin)] <- 1
-# raw$cabin_letter <- as.factor(raw$cabin_letter)
-# raw <- cbind(raw,predict(dummyVars(~cabin_letter, data = raw), newdata = raw))
-# attach(raw);table(Survived,cabin);detach(raw)
-
-raw$Ticket_numeric <- as.numeric(gsub("[^0-9]","",train$Ticket))
+#Ticket
+raw$Ticket_numeric <- as.numeric(gsub("[^0-9]","",raw$Ticket))
 raw$Ticket_numeric[raw$Ticket_numeric==3] <- NA
+raw$Ticket_group[raw$Ticket_numeric < 75000] <- "A"
+raw$Ticket_group[raw$Ticket_numeric > 75000 & raw$Ticket_numeric < 150000] <- "B"
+raw$Ticket_group[raw$Ticket_numeric > 150000 & raw$Ticket_numeric < 300000] <- "C"
+raw$Ticket_group[raw$Ticket_numeric > 300000 ] <- "C"
+raw <- cbind(raw,predict(dummyVars(~Ticket_group, data = raw), newdata = raw))
 
-#Imputing Data
+
 raw$Embarked[raw$Embarked==""] <- NA
 library(caret)
 set.seed(13343)
@@ -35,25 +38,21 @@ raw$child[raw$Age<=10 | (raw$Age <= 15 & raw$Parch>0) | raw$SibSp > 1 | raw$titl
 library(dplyr)
 raw<-tbl_df(raw)
 raw<-raw %>%
-  dplyr::select(-title,-Pclass,-Embarked,-Ticket_numeric) %>%
+  dplyr::select(-title,-Pclass,-Embarked,-Ticket_numeric,-Embarked.) %>%
   dplyr::select(-Sex) %>%
-  #dplyr::select(-Cabin,-Ticket,-Name,cabin_letter.B) %>%
   dplyr::select(Survived,
-                #Sex,
-                contains("title"),contains("Pclass"),cabin,contains("Embarked"),Fare,Parch,familySize,
-                PassengerId) %>%
-  #mutate_at(as.factor) %>%
-  na.omit %>%
+                contains("title"),contains("Pclass"),cabin,contains("Embarked"),contains("group"),Fare,Parch,familySize) %>%
+  na.omit() %>%
   mutate_if(function(col) ifelse(is.factor(col),FALSE,max(col)==1 & min(col)==0),as.factor)
-raw$Fare <- as.numeric(as.character(raw$Fare)) 
-
+#raw$Fare <- as.numeric(as.character(raw$Fare)) 
+raw$Ticket_group <- as.factor(raw$Ticket_group)
 if(linear==FALSE){
   clean <- list() 
-  clean$predictors <- raw %>% dplyr::select(-Survived) 
+  clean$predictors <- raw %>% dplyr::select(-Survived,-contains("group")) 
   clean$Survived <- raw %>% dplyr::select(Survived) %>% mutate_all(as.factor)
 } else if (linear==TRUE){
   raw <- raw %>%
-    dplyr::select(-title.mr,-Pclass.3,-Embarked.S)
+    dplyr::select(-title.mr,-Pclass.3,-Embarked.S,-Ticket_numericC)
   clean <- list() 
   clean$predictors <- raw %>% dplyr::select(-Survived) 
   clean$Survived <- raw %>% dplyr::select(Survived) %>% mutate_all(as.factor)
