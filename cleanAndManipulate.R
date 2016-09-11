8e8fb906f298f89a7fc703aa0ab647359b354979
askilton1 <antonio.skilton@gmail.com>
2016-09-10 22:25:54 -0700

    now cleanAndManipulate.R accomodates linear models by omitting control dummy vars

--- cleanAndManipulate.R
+++ cleanAndManipulate.R
@@ -1,5 +1,4 @@
-cleanAndManipulate <- function(raw){
-
+cleanAndManipulate <- function(raw,linear=FALSE){
 raw$familySize <- raw$Parch + raw$SibSp
   
 raw$title <- NA
@@ -11,13 +10,15 @@ raw <- cbind(raw,predict(dummyVars(~title, data = raw), newdata = raw))
 raw$Pclass <- as.factor(raw$Pclass);raw <- cbind(raw,predict(dummyVars(~Pclass, data = raw), newdata = raw))
 raw <- cbind(raw,predict(dummyVars(~Embarked, data = raw), newdata = raw))
 
-raw$cabin_letter <- "U"
-for(letter in c("A","B","C","D","E","F","T","G")) raw$cabin_letter[grepl(letter,raw$Cabin)] <- letter
-raw$cabin_letter <- as.factor(raw$cabin_letter)
-raw <- cbind(raw,predict(dummyVars(~cabin_letter, data = raw), newdata = raw))
-#attach(raw);table(Survived,cabin_letter);detach(raw)
+raw$cabin <- 0
+raw$cabin[raw$Cabin==""] <- 1
+#for(letter in c("A","B","C","D","E","F","T","G")) raw$cabin[grepl(letter,raw$Cabin)] <- 1
+# raw$cabin_letter <- as.factor(raw$cabin_letter)
+# raw <- cbind(raw,predict(dummyVars(~cabin_letter, data = raw), newdata = raw))
+# attach(raw);table(Survived,cabin);detach(raw)
 
-
+raw$Ticket_numeric <- as.numeric(gsub("[^0-9]","",train$Ticket))
+raw$Ticket_numeric[raw$Ticket_numeric==3] <- NA
 
 #Imputing Data
 raw$Embarked[raw$Embarked==""] <- NA
@@ -34,20 +35,28 @@ raw$child[raw$Age<=10 | (raw$Age <= 15 & raw$Parch>0) | raw$SibSp > 1 | raw$titl
 library(dplyr)
 raw<-tbl_df(raw)
 raw<-raw %>%
-  dplyr::select(-title,-Pclass,-cabin_letter.G,-cabin_letter.A,-cabin_letter.T,-cabin_letter.C,-cabin_letter,-Embarked) %>%
+  dplyr::select(-title,-Pclass,-Embarked,-Ticket_numeric) %>%
   dplyr::select(-Sex) %>%
   #dplyr::select(-Cabin,-Ticket,-Name,cabin_letter.B) %>%
   dplyr::select(Survived,
-         #Sex,
-         contains("title"),contains("Pclass"),contains("cabin_letter"),contains("Embarked"),Fare,Parch,familySize,
-         PassengerId) %>%
+                #Sex,
+                contains("title"),contains("Pclass"),cabin,contains("Embarked"),Fare,Parch,familySize,
+                PassengerId) %>%
   #mutate_at(as.factor) %>%
   na.omit %>%
   mutate_if(function(col) ifelse(is.factor(col),FALSE,max(col)==1 & min(col)==0),as.factor)
 raw$Fare <- as.numeric(as.character(raw$Fare)) 
 
-clean <- list() 
-clean$predictors <- raw %>% dplyr::select(-Survived) 
-clean$Survived <- raw %>% dplyr::select(Survived) %>% mutate_all(as.factor) 
-
-return(clean)} 
+if(linear==FALSE){
+  clean <- list() 
+  clean$predictors <- raw %>% dplyr::select(-Survived) 
+  clean$Survived <- raw %>% dplyr::select(Survived) %>% mutate_all(as.factor)
+} else if (linear==TRUE){
+  raw <- raw %>%
+    dplyr::select(-title.mr,-Pclass.3,-Embarked.S)
+  clean <- list() 
+  clean$predictors <- raw %>% dplyr::select(-Survived) 
+  clean$Survived <- raw %>% dplyr::select(Survived) %>% mutate_all(as.factor)
+}
+return(clean)
+} 
